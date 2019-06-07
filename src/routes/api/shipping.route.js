a926b3af5dce7cc73169cecbcff3aaaec1dc2b31
Peter Adeoye <cwizard2011@gmail.com>
2019-06-07 19:56:37 +0100

    remove redis and update .env.example description

--- src/routes/api/shipping.route.js
+++ src/routes/api/shipping.route.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import ShippingController from '../../controllers/shipping.controller';
-import redisCache from '../../middlewares/redisCache';
 
 const router = Router();
 
-router.get('/shipping/regions', redisCache, ShippingController.getShippingRegions);
-router.get('/shipping/regions/:shipping_region_id', redisCache, ShippingController.getShippingType);
+router.get('/shipping/regions', ShippingController.getShippingRegions);
+router.get('/shipping/regions/:shipping_region_id', ShippingController.getShippingType);
 
 export default router;
